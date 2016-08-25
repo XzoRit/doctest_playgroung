917c72391d7ca995e9725d8fff6353253e4eddf1
XzoRit <xzorit@hotmail.com>
2016-08-25 21:31:18 +0200

    either tests of lib or main can be excuted

--- playground/lib/calculator.cpp
+++ playground/lib/calculator.cpp
@@ -7,3 +7,18 @@ int add (int a, int b)
     return a + b;
 }
 }
+
+
+#include "doctest.h"
+
+namespace calculator::test
+{
+TEST_SUITE("calculator");
+
+TEST_CASE("add")
+{
+    CHECK(add(1, 2) == 3);
+}
+
+TEST_SUITE_END();
+}
--- playground/lib/calculator.hpp
+++ playground/lib/calculator.hpp
@@ -1,22 +1,6 @@
 #pragma once
 
-#include "doctest.h"
-
 namespace calculator
 {
 int add(int a, int b);
-}
-
-#include "doctest.h"
-
-namespace calculator::test
-{
-TEST_SUITE("calculator");
-
-TEST_CASE("add")
-{
-    CHECK(add(1, 2) == 3);
-}
-
-TEST_SUITE_END();
 }
