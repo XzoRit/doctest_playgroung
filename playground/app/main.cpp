398069d0754c47af038f3e1f6dbcc108c8e6c747
XzoRit <xzorit@hotmail.com>
2016-08-28 10:12:24 +0200

    using date portion of lib aswell

--- playground/app/main.cpp
+++ playground/app/main.cpp
@@ -1,10 +1,12 @@
 #include "lib/calculator.hpp"
+#include "lib/leap_year.hpp"
 #include <iostream>
 
 int main(int argc, char* argv[])
 {
     std::cout << "\n******************\n\n";
     std::cout << "calculator::add(1, 2) = " << calculator::add(1, 2) << '\n';
+    std::cout << "date::is_leap_year(1234) = " << date::is_leap_year(1234) << '\n';
     std::cout << "\n******************\n";
 
     return 0;
